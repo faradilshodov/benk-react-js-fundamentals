a3f64ecd1f152382b5e73c7fae0baf9975e883a3
faradilshodov <farrukh.dilshodov@gmail.com>
2024-03-05 13:32:47 -0500

    Create a route and components

---
+++ src/app/coffeetext/componentTwo.tsx
@@ -0,0 +1,11 @@
+export default function CoffeeTextComponent() {
+    return <p>I am component two</p>
+}
+
+export function ComponentThree() {
+    return <p>I am component three</p>
+}
+
+export function ComponentFour() {
+    return <p>I am component four</p>
+}
--- src/app/coffeetext/page.tsx
+++ src/app/coffeetext/page.tsx
@@ -1,3 +1,13 @@
+import ComponentTwo from "./componentTwo";
+import { ComponentThree, ComponentFour } from "./componentTwo";
+
 export default function CoffeeTextComponent(){
-    return <p>Coffee Text List Will Go Here</p>
+    return (
+        <>
+            <p>Coffee Text List Will Go Here</p>
+            <ComponentTwo/>
+            <ComponentThree/>
+            <ComponentFour/>
+        </>
+    )
 }
